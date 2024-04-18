678605153526a2e7bbc0affbcc2cf66726ea2434
DongHyuk <72674324+returndeneb@users.noreply.github.com>
2024-04-18 17:34:52 +0900

    다시 뒤집었다

--- Assets/FaceMesh/Script/EyeRegion.cs
+++ Assets/FaceMesh/Script/EyeRegion.cs
@@ -5,13 +5,17 @@ namespace MediaPipe.FaceMesh
     sealed class EyeRegion
     {
         public float4x4 CropMatrix { get; private set; }
-
+        bool _flipped;
+        public EyeRegion(bool flipped = false)
+            => _flipped = flipped;
         public void Update(float2 p0, float2 p1, float4x4 rotation)
         {
             var box = BoundingBox.CenterExtent
                 ((p0 + p1) * 0.5f, math.distance(p0, p1) * 1.4f);
            
             CropMatrix = math.mul(box.CropMatrix, rotation);
+            if (_flipped)
+                CropMatrix = math.mul(CropMatrix, MathUtil.HorizontalFlip());
         }
 
     }
